67342f6f583ca650449891ea39025b739c6fdf4d
Matt Thommes <matt@activecampaign.com>
2017-04-16 10:45:58 -0500

    Initial Yahoo class file for OAuth stuff.

---
+++ src/Rest/Yahoo.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MattThommes\Rest;
+
+use MattThommes\Debug;
+use MattThommes\Rest\Http;
+
+class Yahoo {
+
+	public $request_url = "https://api.login.yahoo.com/oauth/v2/get_request_token";
+	public $access_token_url = "https://api.login.yahoo.com/oauth/v2/get_token";
+
+}
+
+?>
