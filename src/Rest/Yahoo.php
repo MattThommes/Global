e650fd664ebb480baee6073856a21bc0bbfdae6f
Matt Thommes <mattthommes@Matts-MacBook-Air.local>
2018-04-15 06:38:43 -0500

    Overhauled to work with Yahoo OAuth2

--- src/Rest/Yahoo.php
+++ src/Rest/Yahoo.php
@@ -5,95 +5,127 @@ namespace MattThommes\Rest;
 use MattThommes\Debug;
 use MattThommes\Rest\Http;
 
-class Yahoo {
-
-	private $d;
+class Yahoo
+{
 	private $auth_consumer_key;
 	private $auth_consumer_secret;
-	private $nonce;
-	private $timestamp;
-	private $signature_method;
-	private $version;
-	private $signature;
-	public $request_url = "https://api.login.yahoo.com/oauth/v2/get_request_token";
-	public $authorize_url = "";
-	public $access_token_url = "https://api.login.yahoo.com/oauth/v2/get_token";
+	private $auth_callback;
+	private $authorize_url;
+	private $access_token;
 
-	function __construct($consumer_key, $consumer_secret, $auth_callback = "oob") {
-		if (!session_id()) {
-			session_start();
-		}
-		$this->d = new Debug;
+	public $auth_url = "https://api.login.yahoo.com/oauth2/request_auth";
+	public $access_token_url = "https://api.login.yahoo.com/oauth2/get_token";
+
+	/**
+	 * @param  string  consumer_key     The Yahoo app client ID
+	 * @param  string  consumer_secret  The Yahoo app client secret
+	 * @param  string  auth_callback    The callback URL or "oob" for "out of band" (meaning a auth code will be presented after authorizing)
+	 * @param  string  access_token     Existing access token
+	 */
+	public function __construct($consumer_key, $consumer_secret, $auth_callback = "oob", $access_token = "")
+	{
 		$this->auth_consumer_key = $consumer_key;
 		$this->auth_consumer_secret = $consumer_secret;
-		$this->nonce = rand(0, 999999);
-		$this->timestamp = time();
-		$this->signature_method = "HMAC-SHA1";
-		$this->version = "1.0";
 		$this->auth_callback = $auth_callback;
-		$this->signature = $_SESSION["signature"] = $this->generateSignature($this->request_url);
-		if (isset($_GET["oauth_token"]) && isset($_GET["oauth_verifier"])) {
-			// Coming back to this page after authorizing on Yahoo.
-			$this->verifyRequest($_GET["oauth_token"], $_GET["oauth_verifier"]);
-		} else {
-			// Start a new OAuth request.
-			$this->authRequest();
+		$this->auth_header = base64_encode(sprintf("%s:%s", $this->auth_consumer_key, $this->auth_consumer_secret));
+
+		$this->access_token = $access_token;
+
+		if (! $this->access_token) {
+			// No access token: start auth process
+			if (isset($_GET["code"])) {
+				// Coming back to this page after authorizing on Yahoo.
+				$this->requestAccessToken($_GET["code"]);
+			} else {
+				// Start a new OAuth request.
+				$this->authRequest();
+			}
 		}
 	}
 
 	/**
-	 * Generate the signature string.
+	 * Get the authorize URL to start the OAuth process
+	 */
+	public function getAuthorizeUrl()
+	{
+		return $this->authorize_url;
+	}
+
+	/**
+	 * Get the access token associated with this class instance
+	 */
+	public function getAccessToken()
+	{
+		return $this->access_token;
+	}
+
+	/**
+	 * Get an authorization URL to authorize access
 	 *
-	 * @param  string base_url The URL used for generating the signature.
-	 * @return string          The complete signature.
+	 * @return void
 	 */
-	private function generateSignature($base_url) {
-		$base = array(
-			"xoauth_lang_pref" => "en-us",
-			"oauth_callback" => $this->auth_callback,
-			"oauth_consumer_key" => $this->auth_consumer_key,
-			"oauth_nonce" => $this->nonce,
-			"oauth_signature_method" => $this->signature_method,
-			"oauth_timestamp" => $this->timestamp,
-			"oauth_version" => $this->version,
-		);
-		$base_keys = array_keys($base);
-		ksort($base_keys, SORT_STRING);
-		$base_key_values = array();
-		foreach ($base_keys as $k) {
-			$base_key_values[] = rawurlencode($k) . "=" . rawurlencode($base[$k]);
-		}
-		$base_str = implode("&", $base_key_values);
-		$base_str = sprintf("%s&%s&%s", rawurlencode("GET"), rawurlencode($base_url), rawurlencode($base_str));
-		$key = sprintf("%s&", $this->auth_consumer_secret);
-		$signature = base64_encode(hash_hmac("sha1", $base_str, $key, true));
-		return $signature;
+	private function authRequest()
+	{
+		$url_params = [
+			"client_id" => $this->auth_consumer_key,
+			"redirect_uri" => urlencode($this->auth_callback),
+			"response_type" => "code",
+			"language" => "en-us",
+		];
+		$this->authorize_url = Http::buildQuery($this->auth_url, $url_params);
 	}
 
 	/**
-	 * Signing Requests, and Getting a Request Token. 
+	 * Exchange authorization code for an access token (short-lived: 1 hour),
+	 * then get the long-lived access token by calling the same function.
+	 *
+	 * @param  string  code     The code returned from Yahoo after authorizing the app, or the refresh token
+	 * @param  boolean refresh  Whether or not a token refresh is happening
+	 * @return mixed
 	 */
-	private function authRequest() {
-		$url_params = array(
-			"oauth_nonce" => $this->nonce,
-			"oauth_timestamp" => $this->timestamp,
-			"oauth_consumer_key" => $this->auth_consumer_key,
-			"oauth_signature_method" => $this->signature_method,
-			"oauth_version" => $this->version,
-			"oauth_signature" => $this->signature,
-			"oauth_callback" => rawurlencode($this->auth_callback),
-		);
-		$url = Http::buildQuery($this->request_url, $url_params);
+	private function requestAccessToken($code, $refresh = false)
+	{
+		$url_params = [
+			"grant_type" => "authorization_code",
+			"redirect_uri" => $this->auth_callback,
+		];
+
+		if ($refresh) {
+			$url_params["grant_type"] = "refresh_token";
+			$url_params["refresh_token"] = $code;
+		} else {
+			$url_params["code"] = $code;
+		}
+
+		$headers = [
+			"Authorization: Basic " . $this->auth_header,
+			"Content-Type: application/x-www-form-urlencoded",
+		];
+
 		$req = new Http;
-		$res = $req->curl($url);
-		$res_vars = explode("&", $res);
-		list(,$oauth_token) = explode("=", $res_vars[1]);
-		$url_params = array(
-			"oauth_token" => $oauth_token,
-			"perms" => "read",
-		);
-		$this->authorize_url = Http::buildQuery($this->authorize_url, $url_params);
-		return true;
+		$res = $req->curl($this->access_token_url, $url_params, "POST", [], $headers);
+
+		// Exchange refresh token for new access token.
+		// This gives us a longer-lived access token.
+		
+		if (! $this->access_token) {
+			$res = json_decode($res);
+
+			// Set the short-lived (one hour) access token
+			$this->access_token = $res->access_token;
+
+			$res = $this->requestAccessToken($res->refresh_token, true);
+			$res = json_decode($res);
+
+			// Set the long-lived access token
+			$this->access_token = $res->access_token;
+
+			// We're done with access tokens: just return
+			return;
+		}
+
+		// We only get here when requesting the long-lived (refreshed) access token (2nd request)
+		return $res;
 	}
 
 }
